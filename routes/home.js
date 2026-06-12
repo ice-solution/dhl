1d693c22d319b87eca859b82ce8324807746b89a
Leung Keith <icesolution0321@gmail.com>
2026-06-12 19:53:49 +0800

    fixed redirection

--- routes/home.js
+++ routes/home.js
@@ -3,9 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  if (req.session.userId) {
-    return res.redirect('/application');
-  }
   res.render('home');
 });
 
