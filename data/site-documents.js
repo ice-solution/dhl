c8a98b66b916a4c0787128959c3b16c708c8100e
Leung Keith <icesolution0321@gmail.com>
2026-06-30 22:52:02 +0800

    update pdf

--- data/site-documents.js
+++ data/site-documents.js
@@ -1,6 +1,7 @@
 /** Public document links (Google Drive / site assets) */
 const SITE_DOCUMENTS = {
-  tShirtSizeChart: 'https://drive.google.com/file/d/1tPPnG-boMSVcv0fNtJojF5ufO1aterUw/view?usp=drive_link',
+  tShirtSizeChart: 'https://drive.google.com/file/d/1nkC8A9HDidau0shyEa1oKoTvy4UN7lRV/view?usp=drive_link',
+  tourSelection: 'https://drive.google.com/file/d/1fNiwbJr-2t3DaMPkbpU1dJ3uEIFKPuUo/view?usp=drive_link',
   personalInfoCollection: 'https://drive.google.com/file/d/11Hsc5dHFSCv9FnM9zzWhywPToZPtFfy1/view?usp=drive_link',
   dataPrivacyPolicy: 'https://drive.google.com/file/d/1xwb34_wCa-SEyFRO7pbjlX_JWDtmAeUP/view?usp=drive_link',
   gidLookup: 'https://connect.dpdhl.com/openlink/content/page/6447a0e2576d2c112e2d175a',
